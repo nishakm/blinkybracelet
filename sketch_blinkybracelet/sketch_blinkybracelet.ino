a730c6503ed366827919d77ae815a8c4de2b9f81
nisha <nishakumarx@gmail.com>
2017-01-02 12:41:51 -0800

    Sketch can now turn on any combination of LEDs
    This still uses a 2x2 matrix but can be expanded
    to add more.
    Right now, there is only one combination it can
    turn on and it is hardcoded.
    
    Signed-off-by: nisha <nishakumarx@gmail.com>

--- sketch_blinkybracelet/sketch_blinkybracelet.ino
+++ sketch_blinkybracelet/sketch_blinkybracelet.ino
@@ -7,36 +7,66 @@
  * nishakumarx@gmail.com
  */
 
-// struct to store led pin high and pin low
+/*
+ * struct to store led pin high and pin low
+ */
 typedef struct{
   int pinHigh;
   int pinLow;
+  int pinHiImp; // we do this because we are charlieplexing
 } LED;
 
-// array of leds
-// This is assuming the LEDs look like this
-// 1 2
-// 3 4
-LED leds[4] = {{13, 11}, {13, 10}, {12, 11}, {12, 10}};
+/* 
+ * Array of leds 
+ * This is assuming the LEDs look like this
+ * 1 2
+ * 3 4
+ */
+LED leds[4] = {{9, 10, 11}, {10, 9, 11},
+               {10, 11, 9}, {11, 10, 9}};
+
+/*
+ * Reset all LEDs 
+ * This means turn all pins into
+ * OUTPUT and bring them down to LOW
+ */
+void reset()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW); 
+}
+
+/*
+ * Turn on a list of LEDs 
+ * We need to do a list because the LEDs
+ * are all tied together
+ */
+void turnOn(int indexArray[], int no)
+{ 
+  for (int i=0; i<no; i++)
+  {
+    int ledNo = indexArray[i]-1;
+    // set pinmode
+    pinMode(leds[ledNo].pinHigh, OUTPUT);
+    pinMode(leds[ledNo].pinLow, OUTPUT);
+    pinMode(leds[ledNo].pinHiImp, INPUT);
+    // turn it on
+    digitalWrite(leds[ledNo].pinLow, LOW);
+    digitalWrite(leds[ledNo].pinHigh, HIGH);
+    delay(1);
+  }
+}
 
 void setup() {
-  // initialize digital PWM pins
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-
+  reset();
 }
 
 void loop() {
-  // This runs in a loop
-  for (int i=0; i<4; i++){
-    //bring the pinHigh high and the pinLow low
-    int pinHigh = leds[i].pinHigh;
-    int pinLow = leds[i].pinLow;
-    digitalWrite(pinHigh, HIGH);
-    digitalWrite(pinLow, LOW);
-    delay(1000);
-    digitalWrite(pinHigh, LOW);
-  }
+  // Turn on 1 and 4
+  int onLeds[] = {1, 4};
+  turnOn(onLeds, sizeof(onLeds));
 }
